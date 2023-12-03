a04582bef455ef808b75fcd1b2396ed40af0ae5a
Andrew Mackie <mackie.andrewcm@gmail.com>
2023-12-03 22:42:51 +0000

    And solve part 2

--- 2/solve.py
+++ 2/solve.py
@@ -2,7 +2,7 @@ import os
 import re
 import numpy as np
 
-def solve(input):
+def solve_part1(input):
     total = 0
     for id,line in enumerate(input):
         blue = np.array(re.findall('(\d*) blue', line),dtype=int)
@@ -14,8 +14,19 @@ def solve(input):
         
     return total
 
+def solve_part2(input):
+    total = 0
+    for id,line in enumerate(input):
+        blue = np.array(re.findall('(\d*) blue', line),dtype=int)
+        red = np.array(re.findall('(\d*) red', line),dtype=int)
+        green = np.array(re.findall('(\d*) green', line),dtype=int)
+        total += np.max(blue) * np.max(red) * np.max(green)
+        
+    return total
 
 if __name__ == '__main__':
     with open('/'.join([os.path.dirname(os.path.abspath(__file__)),'input.txt'])) as input:
-        print(solve(input))
+        print(solve_part1(input))
+    with open('/'.join([os.path.dirname(os.path.abspath(__file__)),'input.txt'])) as input:
+        print(solve_part2(input))
         
