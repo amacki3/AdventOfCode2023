228ad7193b8fcfa97b3ffaa46dbf0986675ec713
Andrew Mackie <mackie.andrewcm@gmail.com>
2023-12-03 21:42:24 +0000

    Solver for both parts

--- 1/solve.py
+++ 1/solve.py
@@ -1,32 +1,38 @@
 import os
 import re
 
-def solve_part1(input):
+def solve(input,text_tokens=False):
+
+    look_up_dict = {'one':'1','two':'2','three':'3','four':'4','five':'5','six':'6','seven':'7','eight':'8','nine':'9',
+    '1':'1','2':'2','3':'3','4':'4','5':'5','6':'6','7':'7','8':'8','9':'9'}
     total = 0
+    digits = [0,0]
     for line in input.split('\n'):
-        print(line)
-        digits = re.findall('\d',line)
-        if (len(digits) < 1):
-            digits = ['0']
-        #Note this works for case where only one number, as start is also end.
-        total += int(digits[0]+digits[-1])
+        search_values = "|"
+        if text_tokens:
+            search_values = "|one|two|three|four|five|six|seven|eight|nine|"
+        locs = re.search(search_values[1:] + "\d",line)
+        if locs != None:
+
+            digits[0] = line[locs.span()[0]:locs.span()[1]]
+            search_values = search_values[::-1]
+
+            locs = re.search(search_values[1:] + "\d",line[::-1])
+
+            digits[1] = line[::-1] [locs.span()[0]:locs.span()[1]] [::-1]
+
+            #Note this works for case where only one number, as start is also end.
+            total += int(look_up_dict[digits[0]]+look_up_dict[digits[1]])
 
     return total
 
-def solve_part2(input):
-    #Use part 1 solver, but first massage input
-    #order . e.g. eightwo needs to be eight, but twoneight needs to be two etc.
-    num_dict = {'one':'1','two':'2','three':'3','four':'4','five':'5','six':'6','seven':'7','eight':'8','nine':'9'}
-    for k,v in num_dict.items():
-        input = input.replace(k,v)
-    return solve_part1(input)
 
 
 if __name__ == '__main__':
     with open('/'.join([os.path.dirname(os.path.abspath(__file__)),'input.txt'])) as input:
         txt = input.read()
-        p1 = solve_part1(txt)
-        p2 = solve_part2(txt)
+        p1 = solve(txt,text_tokens=False)
+        p2 = solve(txt,text_tokens=True)
     print("Part 1:")
     print(p1)
     print("Part 2:")
