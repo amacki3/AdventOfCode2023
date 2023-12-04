d4a8db8dfcc1942f14de5a441562f51d3859b43b
Andrew Mackie <mackie.andrewcm@gmail.com>
2023-12-04 00:09:28 +0000

    Solve part 2 day 3

--- 3/solve.py
+++ 3/solve.py
@@ -3,7 +3,6 @@ import re
 
 def check_token(tok):
     if tok not in ['.','0','1','2','3','4','5','6','7','8','9','\n']:
-        print(tok)
         return False
     return True
 
@@ -54,9 +53,71 @@ def solve(input):
 
     return total
 
-                    
-                    
+def getNum(line,start):
+
+    numStr = line[start]
+    curIndex = start-1
+    while(curIndex >= 0 and line[curIndex] in ['0','1','2','3','4','5','6','7','8','9']):
+        numStr = line[curIndex] + numStr
+        curIndex -=1
+    curIndex = start+1
+    while(curIndex <= len(line) and line[curIndex] in ['0','1','2','3','4','5','6','7','8','9']):
+        numStr = numStr + line[curIndex]
+        curIndex += 1
+    return int(numStr)
+
+def solve_part2(input):
+    lines = input.readlines()
+    total = 0
+    maxLines = len(lines)
+
+    nums = ['0','1','2','3','4','5','6','7','8','9']
+
+    for lineId,data in enumerate(lines):
+        for columnId,tok in enumerate(data):
+            if tok == '*':
+                numGears = 0
+                possibleGearNum = 1
+                #first check above it
+                if not lineId == 0:
+                    if lines[lineId-1][columnId] in nums:
+                        numGears += 1
+                        possibleGearNum = possibleGearNum * getNum(lines[lineId-1],columnId)
+                    else:
+                        if lines[lineId-1][columnId-1] in nums:
+                            numGears += 1
+                            possibleGearNum = possibleGearNum * getNum(lines[lineId-1],columnId-1)
+                        if lines[lineId-1][columnId+1] in nums:
+                            numGears += 1
+                            possibleGearNum = possibleGearNum * getNum(lines[lineId-1],columnId+1)
+                #Now check below it
+                if not (lineId+1> maxLines):
+                    if lines[lineId+1][columnId] in nums:
+                        numGears += 1
+                        possibleGearNum = possibleGearNum * getNum(lines[lineId+1],columnId)
+                    else:
+                        if lines[lineId+1][columnId-1] in nums:
+                            numGears += 1
+                            possibleGearNum = possibleGearNum * getNum(lines[lineId+1],columnId-1)
+                        if lines[lineId+1][columnId+1] in nums:
+                            numGears += 1
+                            possibleGearNum = possibleGearNum * getNum(lines[lineId+1],columnId+1)                     
+
+                #And next to it
+                if lines[lineId][columnId-1] in nums:
+                    numGears += 1
+                    possibleGearNum = possibleGearNum * getNum(lines[lineId],columnId-1)
+                if lines[lineId][columnId+1] in nums:
+                    numGears += 1
+                    possibleGearNum = possibleGearNum * getNum(lines[lineId],columnId+1) 
+
+                if numGears == 2:
+                    total += possibleGearNum
+    return total             
+
 
 if __name__ == '__main__':
     with open('/'.join([os.path.dirname(os.path.abspath(__file__)),'input.txt'])) as input:
         print(solve(input))
+    with open('/'.join([os.path.dirname(os.path.abspath(__file__)),'input.txt'])) as input:
+        print(solve_part2(input))
