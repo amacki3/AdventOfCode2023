569e13a2579e822b2e660c0d6a74633199164830
Andrew Mackie <mackie.andrewcm@gmail.com>
2023-12-06 22:27:34 +0000

    day 5 part 2 attempt 3

--- 5/solve.py
+++ 5/solve.py
@@ -8,7 +8,10 @@ class router:
         self.source = np.int64(source)
         self.destination = np.int64(destination)
         self.range_value = np.int64(range_value)
-        
+        #Below for alt part 2
+        self.delta = np.int64(destination - source)
+        self.end = np.int64(source + range_value - 1)
+        self.start = np.int64(source)
     def apply(self,val):
         return val + (self.destination-self.source)
 
@@ -78,11 +81,90 @@ def solve_part2(input):
         min_location = min(min_location, determine_min_location(seeds,maps))
     return min_location
 
+
+class seed_range:
+    def __init__(self,seed,r):
+        self.start = np.int64(seed)
+        self.range = np.int64(r)
+        self.end = np.int64(seed + r - 1)
+
+def solve_part2_alt(input):
+    seed_parsed,maps = parse_input(input)
+    seed_start = seed_parsed[::2]
+    seed_range_nums = seed_parsed[1::2]
+
+    seeds = []
+
+    for s,r in zip(seed_start,seed_range_nums):
+        seeds.append(seed_range(s,r))
+        seeds= sorted(seeds,key=lambda x: x.start)
+
+    # for s in seed_parsed:
+    #     seeds.append(seed_range(s,1))
+
+    #preprocessing complete. now lets do it
+    for routes in maps:
+        new_seeds = []
+        print("NEW ROUTE SET")
+        routeid = 0
+        #seeds are sorted.
+        #routes are sorted
+        for seed in seeds:
+            routeid = 0
+            #find the first route that is appropriate and work forward
+            found = False
+            while(routeid < len(routes) and not found):
+                if seed.start > routes[routeid].end:
+                    routeid += 1
+                else:
+                    if seed.end <= routes[routeid].end:
+                        if seed.start >= routes[routeid].start:
+                            #Simple, route encompasses seed. add new seed all changed
+                            new_seeds.append(seed_range(seed.start + routes[routeid].delta,seed.range))
+                            found = True
+                        else:
+                            #Seed start is earlier, so find what needs to happen and we have exhausted this seed.
+                            if seed.end < routes[routeid].start:
+                                new_seeds.append(seed)
+                                found = True
+                            else:
+                                left_range = routes[routeid].start - seed.start
+                                if left_range >= 0:
+                                    new_seeds.append(seed_range(seed.start,left_range))
+                                right_range = seed.range - left_range
+                                if right_range >= 0:
+                                    new_seeds.append(seed_range(routes[routeid].destination,right_range))
+                                found = True
+                    else:
+                        #Route is not fully encompassing, work it out.
+                        if seed.start < routes[routeid].start:
+                            left_range = routes[routeid].start - seed.start
+                            new_seeds.append(seed_range(seed.start,left_range))
+                            #We now know that the right_range is wider than the route, so we add a seed the length of the route
+                            new_seeds.append(seed_range(routes[routeid].destination,routes[routeid].range_value))
+                            #And we have the remaining part of the seed
+                            seed = seed_range(routes[routeid].end+1,seed.end - routes[routeid].end)
+                        else:
+                            #Seed is longer than the route but starts in it
+                            left_range = routes[routeid].end - seed.start
+                            new_seeds.append(seed_range(seed.start,left_range))
+                            seed = seed_range(routes[routeid].end+1,seed.range-left_range)
+
+                    routeid += 1
+            if not found:
+                new_seeds.append(seed)
+        seeds = sorted(new_seeds,key = lambda x: x.start)
+        # seeds = [s for s in seeds if not s.start == 0]
+        sprint = [s.start for s in seeds]
+        print("SEEDS:", sprint)
+
+    return seeds[0].start
+
 if __name__ == '__main__':
     with open('/'.join([os.path.dirname(os.path.abspath(__file__)),'input.txt'])) as input:
         print("Part 1")
         print(solve_part1(input))
     with open('/'.join([os.path.dirname(os.path.abspath(__file__)),'input.txt'])) as input:
         print("Part 2")
-        print(solve_part2(input))
+        print(solve_part2_alt(input))
         
